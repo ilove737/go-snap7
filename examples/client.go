8b4038e14d2f54c7dfb0e6bdb916716f88b74c0d
李小松 <rj45usb@163.com>
2022-11-15 00:15:05 +0800

    添加客户端示例
    Signed-off-by: 李小松 <rj45usb@163.com>

---
+++ examples/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	s7 "github.com/ilove737/go-snap7"
+)
+
+func main() {
+
+	Client := s7.Cli_Create()
+	// C.Cli_SetAsCallback(Client, CliCompletion,NULL);
+	// C.Cli_ConnectTo(Client, C.CString("127.0.0.1"), 0, 2)
+	Client.Cli_ConnectTo("127.0.0.1", 0, 2)
+	CP := Client.Cli_GetCpInfo()
+	fmt.Println(CP.MaxPduLengt)
+
+	CPU := Client.Cli_GetCpuInfo()
+	fmt.Println(CPU.ModuleTypeName)
+
+	fmt.Println(Client.Cli_GetExecTime())
+
+	var wdata []byte
+	for i := 0; i < 48; i++ {
+		wdata = append(wdata, byte(i))
+	}
+
+	Client.Cli_DBWrite(1, 0, 48, wdata)
+
+	fmt.Println(Client.Cli_DBRead(1, 1, 32))
+
+	Client.Cli_MBWrite(1, 20, wdata)
+	fmt.Println(Client.Cli_MBRead(1, 20))
+
+}
---
+++ examples/go.mod
@@ -0,0 +1,5 @@
+module examples
+
+go 1.18
+
+require github.com/ilove737/go-snap7 v0.0.0-20221114160727-513f179fddce
